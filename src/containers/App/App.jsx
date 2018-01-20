45fabce2a76f01a6660fb080b92719cc751f975f
Ningersan <ningersan@gmail.com>
2018-01-20 17:44:33 +0800

    initial commit

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Login from '../Login/Login'
+import Home from '../Home/Home'
 import styles from './App.scss'
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
         return (
             <MuiThemeProvider>
                 <div className={styles.container}>
-                    <Login />
+                    <Home />
                 </div>
             </MuiThemeProvider>
         )
