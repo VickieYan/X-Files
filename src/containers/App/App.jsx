90bda75ec8e19595296605d00a0c40add4bac70b
Ningersan <ningersan@gmail.com>
2018-01-23 14:57:04 +0800

    initial

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Login from '../Login/Login'
 import Home from '../Home/index'
+import Edit from '../Edit/Edit'
 import styles from './App.scss'
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
         return (
             <MuiThemeProvider>
                 <div className={styles.container}>
-                    <Home />
+                    <Edit />
                 </div>
             </MuiThemeProvider>
         )
---
+++ src/containers/Edit/Edit.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Edit extends Component {
+    render() {
+        return <div>hello, world</div>
+    }
+}
+
+export default Edit
